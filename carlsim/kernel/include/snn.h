93d62e2a5803f2518c5377fc62b0553a0e342151
Michael Beyeler <mbeyeler@uci.edu>
2015-12-14 13:48:23 -0800

    update MINOR_VERSION

--- carlsim/kernel/include/snn.h
+++ carlsim/kernel/include/snn.h
@@ -122,7 +122,7 @@ public:
 	// +++++ PUBLIC PROPERTIES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
 
 	const static unsigned int MAJOR_VERSION = 3; //!< major release version, as in CARLsim X
-	const static unsigned int MINOR_VERSION = 0; //!< minor release version, as in CARLsim 2.X
+	const static unsigned int MINOR_VERSION = 1; //!< minor release version, as in CARLsim 2.X
 
 
 
